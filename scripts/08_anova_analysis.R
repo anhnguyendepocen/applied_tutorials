afb96e07c6cbb366928877bdfe1d37e3a4324e1e
Leon Jessen <leon.jessen@dbac.dk>
2017-06-06 13:17:06 +0200

    Update comments on anova analysis

--- scripts/08_anova_analysis.R
+++ scripts/08_anova_analysis.R
@@ -6,15 +6,24 @@ rm(list=ls())
 # ------------------------------------------------------------------------------
 library('tidyverse')
 library('cowplot')
+library('broom')
 
 # Create data
 # ------------------------------------------------------------------------------
+
+# Set seed for random number generation, to ensure reproducibility 
 set.seed(771160)
-a_1 = 1
-a_2 = 2
-X   = tibble(x  = rnorm(100),
-             cl = sample(c(-1,1), 100, replace = TRUE),
-             y  = 0.2*cl + ifelse(cl==1, a_1 * x, a_2 * x) + rnorm(100)) %>% 
+
+# Create linear data: y = a + b * x + e, where
+# Intercept: a = 0.2 * (-1,1)
+# Slope:     b = (b_1,b_2)
+# Residuals: e = random normal numbers
+b_1 = 1
+b_2 = 2
+X   = tibble(x  = rnorm(100), # x-values
+             cl = sample(c(-1,1), 100, replace = TRUE), # class assignment
+             y  = 0.2*cl + ifelse(cl == 1, b_1 * x, b_2 * x) + rnorm(100) # y-values
+             ) %>% 
   mutate(cl = factor(cl))
 
 # View data
@@ -31,19 +40,19 @@ p2 = X %>% ggplot(aes(x = x, y = y, colour = cl)) +
   theme_bw() +
   labs(title = 'Model 2')
 
-plot_grid(p1,p2)
+plot_grid(p1,p2) %>% print
 
-# Do Anova Analysis
+# Do Anova Analysis and display output
 # ------------------------------------------------------------------------------
 # Model 1
 m1 = X %>% lm(formula = y ~ x, data = .)
-summary(m1)
-anova(m1)
+m1 %>% summary %>% print
+m1 %>% anova %>% print
 
 # Model 2
 m2 = X %>% lm(y ~ x * cl, data = .)
-summary(m2)
-anova(m2)
+m2 %>% summary %>% print
+m2 %>% anova %>% print
 
 # Combined
-anova(m1,m2)
+anova(m1,m2) %>% print
